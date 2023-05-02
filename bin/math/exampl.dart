886fa40fca75b3bc7442fc4d4ed5fde43b1319e3
Berlinkg <nadyrbekov570@gmail.com>
2023-05-02 15:22:29 +0600

    Math_dart

---
+++ bin/math/exampl.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+// import 'math.dart';
+// import 'dart:async';
+
+void main() {
+  int num1 = 10;
+  int num2 = 2;
+
+  num maximum = max(num1, num2);
+  num maxinun = max(num1, num2);
+  num minimum = min(num1, num2);
+  num squred = sqrt(25);
+  print(maximum);
+  print(squred);
+}
